a19d4be117294acb52beaa7aa9e60fe04062fe63
alex-dodoka <neksus.92@mail.ru>
2017-06-05 20:55:14 +0300

    Create favorite number

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
-function print(min) {
+function print() {
     let max = +$('#numb').val();
     let res = Math.floor(Math.random() * max);
-    if (res === 3) {
+    let favoritNumber = 3;
+    if (res === favoritNumber) {
         console.log(`Ух ты. Я поймал цифру -${res}- через ECMAScript 6`);
     }
     console.log(res);
